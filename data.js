66c2c20eace965c3280e700e07ce177b9c6966d8
Shella May Cuenca <shellamaycuenca12@example.com>
2025-11-24 23:42:50 +0800

    temporary

--- data.js
+++ data.js
@@ -4,85 +4,99 @@ var people = [
         "artist": "Raphael",
         "picture": "images/artwork10.jpg",
         "year": "c. 1506",
-        "medium": "Oil on panel"
+        "medium": "Oil on panel",
+        "category": "artwork"
     },
     {
         "title": "A Young Woman and Her Little Boy",
         "artist": "Agnolo Bronzino",
         "picture": "images/artwork6.jpg",
         "year": "c. 1540",
-        "medium": "Oil on panel"
+        "medium": "Oil on panel",
+        "category": "artwork"
     },
     {
         "title": "Titian's Schoolmaster",
         "artist": "Giovanni Battista Moroni",
         "picture": "images/artwork1.jpg",
         "year": "c. 1575",
-        "medium": "Oil on canvas"
+        "medium": "Oil on canvas",
+        "category": "artwork"
     },
     {
         "title": "Saint John in the Desert",
         "artist": "Domenico Veneziano",
         "picture": "images/artwork3.jpg",
         "year": "c. 1445/1450",
-        "medium": "Tempera on panel"
-    },
-    {
-        "title": "Charity",
-        "artist": "Mino da Fiesole",
-        "picture": "images/artwork7.jpg",
-        "year": "1475/1480",
-        "medium": "Marble"
+        "medium": "Tempera on panel",
+        "category": "artwork"
     },
     {
         "title": "Saint Luke",
         "artist": "Giorgio Vasari",
         "picture": "images/artwork5.jpg",
         "year": "1570–1571",
-        "medium": "Oil on panel"
+        "medium": "Oil on panel",
+        "category": "artwork"
     },
     {
         "title": "Monsignor della Casa",
         "artist": "Pontormo",
         "picture": "images/artwork2.jpg",
         "year": "probably 1541/1544",
-        "medium": "Oil on panel"
+        "medium": "Oil on panel",
+        "category": "artwork"
+    },
+];
+var sculpture = [
+    {
+        "title": "Charity",
+        "artist": "Mino da Fiesole",
+        "picture": "images/artwork7.jpg",
+        "year": "1475/1480",
+        "medium": "Marble",
+        "category": "sculpture"
+    }, 
+     {
+        "title": "Putto Poised on a Globe",
+        "artist": "Andrea del Verrocchio",
+        "picture": "images/artwork9.jpg",
+        "year": "c. 1480",
+        "medium": "Unbaked clay",
+        "category": "sculpture"
     },
     {
         "title": "Seated Nymph",
         "artist": "Antico",
         "picture": "images/artwork11.jpg",
         "year": "1503",
-        "medium": "Bronze with mercury gilding on drapery and hair, silvering and silver foil in the eyes"
+        "medium": "Bronze with mercury gilding on drapery and hair, silvering and silver foil in the eyes",
+        "category": "sculpture"
     },
-    {
-        "title": "Putto Poised on a Globe",
-        "artist": "Andrea del Verrocchio",
-        "picture": "images/artwork9.jpg",
-        "year": "c. 1480",
-        "medium": "Unbaked clay"
-    },
-    {
+     {
         "title": "Madonna and Child",
         "artist": "Donatello",
         "picture": "images/artwork12.jpg",
         "year": "c. 1420/1425",
-        "medium": "Painted and gilded terracotta"
+        "medium": "Painted and gilded terracotta",
+        "category": "sculpture"
     },
-
-
-    {
-        "title": "Saint Luke",
-        "artist": "Giorgio Vasari",
-        "picture": "images/artwork4.jpg",
-        "year": "1570–1571",
-        "medium": "Oil on panel"
-    },
-    {
-        "title": "Summer",
-        "artist": "Jacopo Tintoretto",
-        "picture": "images/artwork13.jpg",
-        "year": "c. 1546/1548",
-        "medium": "Oil on canvas"
-    }
+    // {
+    //     "title": "Saint Luke",
+    //     "artist": "Giorgio Vasari",
+    //     "picture": "images/artwork4.jpg",
+    //     "year": "1570–1571",
+    //     "medium": "Oil on panel",
+    //     "category": "artwork"
+    // },
+    // {
+    //     "title": "Summer",
+    //     "artist": "Jacopo Tintoretto",
+    //     "picture": "images/artwork13.jpg",
+    //     "year": "c. 1546/1548",
+    //     "medium": "Oil on canvas",
+    //     "category": "artwork"
+    // },
+  
+   
 ];
