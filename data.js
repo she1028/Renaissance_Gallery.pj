a6709a5e1caf76c7e10f5921bdf42ec253866666
Shella May Cuenca <shellamaycuenca12@example.com>
2025-11-25 22:04:51 +0800

    updated

--- data.js
+++ data.js
@@ -5,7 +5,8 @@ var people = [
         "picture": "images/artwork10.jpg",
         "year": "c. 1506",
         "medium": "Oil on panel",
-        "category": "artwork"
+        "category": "artwork",
+         "description": "A dramatic Renaissance scene of Saint George heroically defeating a dragon, symbolizing the triumph of good over evil."
     },
     {
         "title": "A Young Woman and Her Little Boy",
@@ -13,7 +14,8 @@ var people = [
         "picture": "images/artwork6.jpg",
         "year": "c. 1540",
         "medium": "Oil on panel",
-        "category": "artwork"
+        "category": "artwork",
+        "description": "A refined Mannerist portrait capturing a noblewoman and her child with elegance, poise, and detailed realism."
     },
     {
         "title": "Titian's Schoolmaster",
@@ -21,7 +23,8 @@ var people = [
         "picture": "images/artwork1.jpg",
         "year": "c. 1575",
         "medium": "Oil on canvas",
-        "category": "artwork"
+        "category": "artwork",
+        "description": "A realistic and expressive portrait of a schoolmaster, reflecting Moroni’s mastery in portraying character and everyday life."
     },
     {
         "title": "Saint John in the Desert",
@@ -29,7 +32,8 @@ var people = [
         "picture": "images/artwork3.jpg",
         "year": "c. 1445/1450",
         "medium": "Tempera on panel",
-        "category": "artwork"
+        "category": "artwork",
+         "description": "A serene depiction of John the Baptist in a radiant landscape, showcasing early Renaissance clarity and soft color."
     },
     {
         "title": "Saint Luke",
@@ -37,7 +41,8 @@ var people = [
         "picture": "images/artwork5.jpg",
         "year": "1570–1571",
         "medium": "Oil on panel",
-        "category": "artwork"
+        "category": "artwork",
+        "description": "A richly detailed image of Saint Luke, demonstrating Vasari’s expressive Mannerist style and dynamic composition."
     },
     {
         "title": "Monsignor della Casa",
@@ -45,7 +50,8 @@ var people = [
         "picture": "images/artwork2.jpg",
         "year": "probably 1541/1544",
         "medium": "Oil on panel",
-        "category": "artwork"
+        "category": "artwork",
+        "description": "An introspective and powerful portrait marked by Pontormo’s emotional depth and distinctive Mannerist color palette."
     },
 ];
 var sculpture = [
@@ -55,48 +61,130 @@ var sculpture = [
         "picture": "images/artwork7.jpg",
         "year": "1475/1480",
         "medium": "Marble",
-        "category": "sculpture"
-    }, 
-     {
+        "category": "sculpture",
+        "description": "A graceful marble sculpture symbolizing charity and maternal compassion, carved with delicate Renaissance detail."
+    },
+    {
         "title": "Putto Poised on a Globe",
         "artist": "Andrea del Verrocchio",
         "picture": "images/artwork9.jpg",
         "year": "c. 1480",
         "medium": "Unbaked clay",
-        "category": "sculpture"
+        "category": "sculpture",
+        "description": "A lively and playful putto balancing on a globe, showcasing Verrocchio’s mastery of movement and youthful energy."
     },
+
     {
         "title": "Seated Nymph",
         "artist": "Antico",
         "picture": "images/artwork11.jpg",
         "year": "1503",
         "medium": "Bronze with mercury gilding on drapery and hair, silvering and silver foil in the eyes",
-        "category": "sculpture"
+        "category": "sculpture",
+        "description": "A refined bronze figure of a nymph, admired for its classical elegance and finely gilded and silvered ornamentation."
     },
-     {
+    {
         "title": "Madonna and Child",
         "artist": "Donatello",
         "picture": "images/artwork12.jpg",
         "year": "c. 1420/1425",
         "medium": "Painted and gilded terracotta",
-        "category": "sculpture"
+        "category": "sculpture",
+        "description": "A tender and expressive depiction of the Virgin Mary and Christ Child, highlighting Donatello’s warmth and emotional realism."
     },
-    // {
-    //     "title": "Saint Luke",
-    //     "artist": "Giorgio Vasari",
-    //     "picture": "images/artwork4.jpg",
-    //     "year": "1570–1571",
-    //     "medium": "Oil on panel",
-    //     "category": "artwork"
-    // },
-    // {
-    //     "title": "Summer",
-    //     "artist": "Jacopo Tintoretto",
-    //     "picture": "images/artwork13.jpg",
-    //     "year": "c. 1546/1548",
-    //     "medium": "Oil on canvas",
-    //     "category": "artwork"
-    // },
-  
-   
+];
+
+
+var all = [
+    {
+        "title": "Saint George and the Dragon",
+        "artist": "Raphael",
+        "picture": "images/artwork10.jpg",
+        "year": "c. 1506",
+        "medium": "Oil on panel",
+        "category": "artwork",
+         "description": "A dramatic Renaissance scene of Saint George heroically defeating a dragon, symbolizing the triumph of good over evil."
+    },
+    {
+        "title": "A Young Woman and Her Little Boy",
+        "artist": "Agnolo Bronzino",
+        "picture": "images/artwork6.jpg",
+        "year": "c. 1540",
+        "medium": "Oil on panel",
+        "category": "artwork",
+        "description": "A refined Mannerist portrait capturing a noblewoman and her child with elegance, poise, and detailed realism."
+    },
+    {
+        "title": "Titian's Schoolmaster",
+        "artist": "Giovanni Battista Moroni",
+        "picture": "images/artwork1.jpg",
+        "year": "c. 1575",
+        "medium": "Oil on canvas",
+        "category": "artwork",
+        "description": "A realistic and expressive portrait of a schoolmaster, reflecting Moroni’s mastery in portraying character and everyday life."
+    },
+    {
+        "title": "Saint John in the Desert",
+        "artist": "Domenico Veneziano",
+        "picture": "images/artwork3.jpg",
+        "year": "c. 1445/1450",
+        "medium": "Tempera on panel",
+        "category": "artwork",
+         "description": "A serene depiction of John the Baptist in a radiant landscape, showcasing early Renaissance clarity and soft color."
+    },
+    {
+        "title": "Saint Luke",
+        "artist": "Giorgio Vasari",
+        "picture": "images/artwork5.jpg",
+        "year": "1570–1571",
+        "medium": "Oil on panel",
+        "category": "artwork",
+        "description": "A richly detailed image of Saint Luke, demonstrating Vasari’s expressive Mannerist style and dynamic composition."
+    },
+    {
+        "title": "Monsignor della Casa",
+        "artist": "Pontormo",
+        "picture": "images/artwork2.jpg",
+        "year": "probably 1541/1544",
+        "medium": "Oil on panel",
+        "category": "artwork",
+        "description": "An introspective and powerful portrait marked by Pontormo’s emotional depth and distinctive Mannerist color palette."
+    },
+    {
+        "title": "Charity",
+        "artist": "Mino da Fiesole",
+        "picture": "images/artwork7.jpg",
+        "year": "1475/1480",
+        "medium": "Marble",
+        "category": "sculpture",
+        "description": "A graceful marble sculpture symbolizing charity and maternal compassion, carved with delicate Renaissance detail."
+    },
+    {
+        "title": "Putto Poised on a Globe",
+        "artist": "Andrea del Verrocchio",
+        "picture": "images/artwork9.jpg",
+        "year": "c. 1480",
+        "medium": "Unbaked clay",
+        "category": "sculpture",
+        "description": "A lively and playful putto balancing on a globe, showcasing Verrocchio’s mastery of movement and youthful energy."
+    },
+
+    {
+        "title": "Seated Nymph",
+        "artist": "Antico",
+        "picture": "images/artwork11.jpg",
+        "year": "1503",
+        "medium": "Bronze with mercury gilding on drapery and hair, silvering and silver foil in the eyes",
+        "category": "sculpture",
+        "description": "A refined bronze figure of a nymph, admired for its classical elegance and finely gilded and silvered ornamentation."
+    },
+    {
+        "title": "Madonna and Child",
+        "artist": "Donatello",
+        "picture": "images/artwork12.jpg",
+        "year": "c. 1420/1425",
+        "medium": "Painted and gilded terracotta",
+        "category": "sculpture",
+        "description": "A tender and expressive depiction of the Virgin Mary and Christ Child, highlighting Donatello’s warmth and emotional realism."
+    },
 ];
